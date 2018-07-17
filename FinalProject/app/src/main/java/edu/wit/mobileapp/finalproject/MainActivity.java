4f461a017fef1ac35e5648535bd19fc81326c796
HirdrWit <robertjhird@gmail.com>
2018-07-17 14:49:32 -0400

    added settings name and age change functionality

--- FinalProject/app/src/main/java/edu/wit/mobileapp/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/edu/wit/mobileapp/finalproject/MainActivity.java
@@ -1,13 +1,55 @@
 package edu.wit.mobileapp.finalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView name;
+TextView age;
+Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = (TextView) findViewById(R.id.nameDataTextView);
+        age = (TextView) findViewById(R.id.ageDataTextView);
+        btn = (Button) findViewById(R.id.settings);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent intent=new Intent(MainActivity.this,settings.class);
+                startActivityForResult(intent, 2);// Activity is started with requestCode 2
+            }
+        });
+
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        // check if the request code is same as what is passed  here it is 2
+        if(requestCode==2)
+        {
+            String nameMessage=data.getStringExtra("nMess");
+            name.setText(nameMessage);
+            String ageMessage=data.getStringExtra("aMess");
+            age.setText(ageMessage);
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
 }
--- FinalProject/app/src/main/java/edu/wit/mobileapp/finalproject/settings.java
+++ FinalProject/app/src/main/java/edu/wit/mobileapp/finalproject/settings.java
@@ -1,13 +1,41 @@
 package edu.wit.mobileapp.finalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class settings extends AppCompatActivity {
-
+public class settings extends MainActivity {
+    Button save;
+    EditText name;
+    EditText age;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        name=(EditText)findViewById(R.id.namePlainText);
+        age=(EditText)findViewById(R.id.agePlainText);
+        save=(Button)findViewById(R.id.button_save);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                String nameMessage=name.getText().toString();
+                String ageMessage=age.getText().toString();
+                Intent intent=new Intent();
+                intent.putExtra("nMess",nameMessage);
+                intent.putExtra("aMess",ageMessage);
+                setResult(2,intent);
+                finish();//finishing activity
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.second, menu);
+        return true;
     }
 }
